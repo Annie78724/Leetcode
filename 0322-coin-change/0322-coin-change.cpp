69389ac536e31ed79f5a707b6a64c8cc28fdab5a
Annie78724 <92166755+Annie78724@users.noreply.github.com>
2022-11-26 12:01:25 +0530

    Time: 146 ms (62.62%), Space: 14.4 MB (78.57%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int amount,vector<int>&dp){
-        if(amount==0) return 0;
-        if(amount<0) return INT_MAX;
-        if(dp[amount]!=-1) return dp[amount];
+    // int solve(vector<int>& coins, int amount,vector<int>&dp){
+    //     if(amount==0) return 0;
+    //     if(amount<0) return INT_MAX;
+    //     if(dp[amount]!=-1) return dp[amount];
+    //     int mini=INT_MAX;
+    //     for(int i=0;i<coins.size();i++)
+    //     {
+    //         int ans=solve(coins,amount-coins[i],dp);
+    //         if(ans!=INT_MAX)
+    //         mini=min(mini,1+ans);
+    //     }
+    //     return dp[amount]=mini;
+    // }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>dp(amount+1,INT_MAX);
+        dp[0]=0;
         int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++)
+        for(int am=1;am<=amount;am++)
         {
-            int ans=solve(coins,amount-coins[i],dp);
+                    int mini=INT_MAX;
+            for(int i=0;i<coins.size();i++)
+        {
+                if(am-coins[i]>=0)
+                {
+                     int ans=dp[am-coins[i]];
             if(ans!=INT_MAX)
             mini=min(mini,1+ans);
+                }
         }
-        return dp[amount]=mini;
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,-1);
-        int ans=solve(coins,amount,dp);
-        return ans==INT_MAX?-1:ans;
+            dp[am]=mini;
+        }
+        return dp[amount]==INT_MAX?-1:dp[amount];
     }
 };
